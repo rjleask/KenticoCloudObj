ebaa0f3ae2f8a8220e0acb9a0096f72d6740ba26
Jamie Leask <JamieL@kentico.com>
2019-05-20 10:16:48 -0400

    Updates

---
+++ precache-manifest.66f59ae729a635f44543f6407ab70946.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c02e202ffbd0514e9b3a70447b9dd426",
+    "url": "/KenticoCloudObj/index.html"
+  },
+  {
+    "revision": "8f1da0aafd50ce76d950",
+    "url": "/KenticoCloudObj/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "f7244305da3086a98f6b",
+    "url": "/KenticoCloudObj/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "8f1da0aafd50ce76d950",
+    "url": "/KenticoCloudObj/static/js/2.3a0ff87b.chunk.js"
+  },
+  {
+    "revision": "f7244305da3086a98f6b",
+    "url": "/KenticoCloudObj/static/js/main.7d1257a5.chunk.js"
+  },
+  {
+    "revision": "3f93138ec8f13fa52369",
+    "url": "/KenticoCloudObj/static/js/runtime~main.61581a89.js"
+  }
+]);
